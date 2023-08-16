022d78a3b0bc7a58c6477a6ed2106878e5e38305
sakib387 <jucse28.387>
2023-08-17 02:02:27 +0600

    seller nav add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import{Router } from "@angular/router"
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  manuType:string='default';
+   constructor(private route:Router){}
+   ngOnInit(){
+    this.route.events.subscribe((val:any)=>{
+      if(val.url){
+        if(localStorage.getItem('seller')&&val.url.includes('seller')){
+          console.log(val.url)
+        this.manuType='seller';
+        }
+      
+      else{
+        this.manuType='default';
+      }}
+    })
+   }
 }
