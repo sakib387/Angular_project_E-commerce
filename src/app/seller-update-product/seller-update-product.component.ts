602a1b1b605c01781e8509f6703126cd892148cf
sakib387 <jucse28.387>
2023-08-19 01:14:46 +0600

    get item by id

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { product } from '../data-model';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -7,6 +9,18 @@ import { product } from '../data-model';
   styleUrls: ['./seller-update-product.component.css']
 })
 export class SellerUpdateProductComponent {
+  productData!:product;
+  constructor(private route:ActivatedRoute,
+    private product:ProductService
+    
+    ){}
+
+  ngOnInit(){
+    let pId=this.route.snapshot.paramMap.get('id');
+    pId && this.product.getProduct(pId).subscribe((res)=>{
+     this.productData=res;
+    })
+  }
   submitProduct(data:product){
 
   }
