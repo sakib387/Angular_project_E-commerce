39090657d0b4bee0a5b15de2842ee806f33076d1
sakib387 <jucse28.387>
2023-08-18 22:18:55 +0600

    get product from api

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-model';
 
 @Component({
   selector: 'app-seller-home',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-home.component.css']
 })
 export class SellerHomeComponent {
-
+  productList!:product[];
+constructor(private product:ProductService){}
+ngOnInit():void{
+  this.product.productList().subscribe((res)=>{
+   this.productList=res;
+  })
+}
 }
